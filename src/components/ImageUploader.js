56155e686457be3e941890b17c5fcc264ec72a89
Arisema Mezgebe Mihretu <arsemamm@gmail.com>
2019-08-30 16:01:01 +0300

    face-api.js integration

--- src/components/ImageUploader.js
+++ src/components/ImageUploader.js
@@ -5,6 +5,7 @@ import { Typography, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 import Dropzone from "react-dropzone";
+import * as faceapi from "face-api.js";
 
 const useStyles = makeStyles(theme => ({
     dropzone: {
@@ -30,6 +31,40 @@ export default function ImageUploader(props) {
             // assume it is image from input file
             preview_image = URL.createObjectURL(props.preview_image);
         }
+        // console.log(faceapi.nets);
+        const input = document.getElementById('uploadedImage');
+        if(input){
+            Promise.all([
+                // faceapi.loadFaceRecognitionModel('./models'),
+                
+                // faceapi.loadFaceLandmarkModel('./models'),
+                // faceapi.loadSsdMobilenetv1Model('./models')
+                faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+                faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+                faceapi.nets.ssdMobilenetv1.loadFromUri('./models')
+            ]).then(start);
+            // console.log(faceapi.nets.faceRecognitionNet.loadFromUri('./models'));
+            function start(){
+                const detections = faceapi.detectAllFaces(input).withFaceLandmarks().withFaceDescriptors()
+                console.log(detections);
+            }
+            // console.log(faceapi.nets);
+            // faceapi.loadTinyFaceDetectorModel('https://hpssjellis.github.io/face-api.js-for-beginners/')
+            // let canvas = document.createElement('canvas');
+            // canvas.width = input.naturalWidth;
+            // canvas.height = input.naturalHeight;
+
+            // canvas.getContext('2d').drawImage(input, 0, 0);
+            // let image_base64 = canvas.toDataURL('jpg');
+            // console.log("here");
+            // image = faceapi.bufferToImage(input)
+            // if(faceapi.detectAllFaces(image_base64).withFaceLandmarks()){
+            //   console.log(detection);
+            // }
+            // input.faceDetection({complete:function(faces){
+            //     console.log(faces);
+            // }})
+      }
     }
 
     function onDrop(accepted_files) {
@@ -45,6 +80,7 @@ export default function ImageUploader(props) {
                         {preview_image ? (
                             <Grid>
                                 <img
+                                    id = "uploadedImage"
                                     src={preview_image}
                                     alt="Preview"
                                     className={classes.preview_image}
