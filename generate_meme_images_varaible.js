b11808e85afa97c656916cf76cf468c69ee13c6a
Yared Taddese <taddeseyared@gmail.com>
2019-09-23 18:25:12 +0300

    [Improve] Update default meme images with new meme images that perform better swap [Improve] Default meme images filename generator can handle filesystem names order by itself

--- generate_meme_images_varaible.js
+++ generate_meme_images_varaible.js
@@ -9,7 +9,36 @@ const meme_images_exporter = './src/default_meme_images.js';    // script which 
 
 const meme_files = fs.readdirSync(meme_images_folder);
 
-// script content to be written in js file that export the default meme images variable 
+/**
+ * Get file order by extracting the starting number before comma in the filename
+ * @param {string} filename should start by number and comma
+ */
+function get_order(filename) {
+    for (let i = 0;i < filename.length;i++) {
+        if (filename[i] === ',') {
+            return parseInt(filename.slice(0, i+1));// increment index to slice(all strings start from apostrophe(') wierd) 
+        }
+    }
+
+    return null;
+};
+
+// sort files by using the numbers in the filename appropriately
+meme_files.sort(function (a, b) {
+    a_order = get_order(a);
+    b_order = get_order(b);
+
+    if (a_order === null) {
+        if (b_order === null) return 0;
+        else return -1;
+    } else if (b_order === null) {
+        return 1;
+    } else {
+        return a_order - b_order;
+    }
+});
+
+// script content to be written in js file that export the default meme images variable
 const exporter_content =
     `
 const meme_images = ${JSON.stringify(meme_files)};
